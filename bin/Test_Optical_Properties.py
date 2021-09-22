f5b51497fd422e830d7db36c689c672f02eb5943
Ted Letcher <ted.letcher@gmail.com>
2021-09-22 17:07:15 -0400

    Major bug fix in PhotonTrack/RT Code + Housekeeping prior to public release

--- bin/Test_Optical_Properties.py
+++ bin/Test_Optical_Properties.py
@@ -1,27 +1,29 @@
 ## test photon track! ##
 import sys
+#sys.path.append('/Users/rdcrltwl/Desktop/CRRELRTM/CRREL-GOSRT/crrelGOSRT/')
 from crrelGOSRT import PhotonTrack
 import numpy as np
-from math import sin, cos, radians
 import pyvista as pv
 import matplotlib.pyplot as plt
 import os
 
 pwd=os.getcwd()
 
-parentPath='/Users/rdcrltwl/Desktop/UVD_microCT/Feb12/VTK/Pit1_12_3/Snow/FULL/CRREL_MESH.vtk'
-GrainPath='/Users/rdcrltwl/Desktop/UVD_microCT/Feb12/VTK/Pit1_12_3/Snow/GRAINS/'
-OutputName='/Users/rdcrltwl/Desktop/ESC_2021/OpticalProps/Facets/Facets_Props/OpticalProp_Coarse.txt'
+parentPath='/Users/rdcrltwl/Desktop/UVD_microCT/Feb12/VTK/Pit1_10_3redo/Snow/FULL_Bottom/CRREL_MESH.vtk'
+GrainPath='/Users/rdcrltwl/Desktop/UVD_microCT/Feb12/VTK/Pit1_10_3redo/Snow/GRAINS_Bottom/'
+OutputName='/Users/rdcrltwl/Desktop/ESC_2021/OpticalProps/Facets/Facets_Props/Facets_Example1.txt'
+#OutputName='/Users/rdcrltwl/Desktop/ESC_2021/OpticalProps/Virgin_Snow/29_27.txt'
 
 #parentPath='/Users/rdcrltwl/Desktop/UVD_microCT/CRREL Snow/CRREL_20Feb21/CRREL_newsnowf_20Feb21/VTK/newsnowf_20Feb21_17um_Rec/VOI/Snow/FULL/CRREL_MESH.vtk'
 #GrainPath='/Users/rdcrltwl/Desktop/UVD_microCT/CRREL Snow/CRREL_20Feb21/CRREL_newsnowf_20Feb21/VTK/newsnowf_20Feb21_17um_Rec/VOI/Snow/GRAINS/'
 #OutputName='/Users/rdcrltwl/Desktop/CRRELRTM/OpticalProperties/CRREL_newsnowf_20Feb21/TestProps.txt'
 MaterialPath = '/Users/rdcrltwl/Desktop/NewRTM/crrel-snow-rtm/Materials/'
-wavelen='800nm'
+wavelen='900nm'
 VoxelRes='19.88250um'
 
 fig=PhotonTrack.RayTracing_OpticalProperties(parentPath,GrainPath,OutputName,MaterialPath,wavelen,VoxelRes,
-                                         verbose=True,nPhotons=2500,Multi=False,GrainSamples=25,Advanced=True)
+                                         verbose=True,nPhotons=3500,Multi=False,GrainSamples=45,Advanced=True,
+                                         FiceFromDensity=False,straight=False,maxBounce=120)
 
-fig.savefig('/Users/rdcrltwl/Desktop/ESC_2021/OpticalProps/Facets/Facets_Props/OpticalProp_Coarse.png',dpi=90)
+fig.savefig('/Users/rdcrltwl/Desktop/ESC_2021/OpticalProps/Virgin_Snow/Facets_Example1.png',dpi=90)
 plt.show()
